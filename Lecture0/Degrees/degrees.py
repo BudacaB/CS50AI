340c178c4ce01f879dd0e18ab84649518ddfa07d
BudacaB <budaca.b@gmail.com>
2022-01-19 00:41:37 +0200

    second go, review

--- Lecture0/Degrees/degrees.py
+++ Lecture0/Degrees/degrees.py
@@ -111,6 +111,7 @@ def shortest_path(source, target):
             while node.parent is not None:
                 path.append((node.action, node.state))
                 node = node.parent
+            path.reverse()
             return path
         
         explored.add(node.state)
