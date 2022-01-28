5e5e193d13564fd0a1eff5485b6440a192b5ed41
BudacaB <budaca.b@gmail.com>
2022-01-28 20:39:56 +0200

    started tictactoe

--- Lecture0/tictactoe/playground.py
+++ Lecture0/tictactoe/playground.py
@@ -1,23 +1,13 @@
-"""
-Tic Tac Toe Player
-"""
-
-import math
 import copy
+
 
 X = "X"
 O = "O"
 EMPTY = None
 
-
-def initial_state():
-    """
-    Returns starting state of the board.
-    """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
-
+board = [[X, O, X],
+        [O, X, EMPTY],
+        [EMPTY, EMPTY, EMPTY]]
 
 def player(board):
     """
@@ -36,7 +26,6 @@ def player(board):
     else:
         return X
 
-
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
@@ -48,7 +37,6 @@ def actions(board):
                 actions.add((row_idx, cell_idx))
     return actions
 
-
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
@@ -59,31 +47,7 @@ def result(board, action):
     board_copy = copy.deepcopy(board)
     board_copy[action[0]][action[1]] = player(board)
     return board_copy
+    
 
-
-def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
-
-
-def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
-
-
-def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
-    raise NotImplementedError
-
-
-def minimax(board):
-    """
-    Returns the optimal action for the current player on the board.
-    """
-    raise NotImplementedError
+# result(board, (1, 1))
+print(result(board, (1, 2)))
