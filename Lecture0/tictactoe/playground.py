5398818b5a7ccae771a7f592a1cea76dd9edea3c
BudacaB <budaca.b@gmail.com>
2022-01-28 22:41:21 +0200

    winner wip

--- Lecture0/tictactoe/playground.py
+++ Lecture0/tictactoe/playground.py
@@ -5,9 +5,9 @@ X = "X"
 O = "O"
 EMPTY = None
 
-board = [[X, O, X],
-        [O, X, EMPTY],
-        [EMPTY, EMPTY, EMPTY]]
+board = [[X, O, EMPTY],
+        [X, X, O],
+        [X, O, EMPTY]]
 
 def player(board):
     """
@@ -49,5 +49,47 @@ def result(board, action):
     return board_copy
     
 
-# result(board, (1, 1))
-print(result(board, (1, 2)))
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for row_idx, row in enumerate(board):
+        for cell_idx, cell in enumerate(row):
+            if row_idx == 0 and cell_idx == 0:
+                if cell == X:
+                    if board[row_idx][cell_idx + 1] == X and board[row_idx][cell_idx + 2] == X:
+                        return X
+                    elif board[row_idx + 1][cell_idx] == X and board[row_idx + 2][cell_idx] == X:
+                        return X
+                    elif board[row_idx + 1][cell_idx + 1] == X and board[row_idx + 2][cell_idx + 2] == X:
+                        return X
+            if row_idx == 0 and cell_idx == 1:
+                if cell == X:
+                    if board[row_idx + 1][cell_idx] == X and board[row_idx + 2][cell_idx] == X:
+                        return X
+            if row_idx == 0 and cell_idx == 2:
+                if cell == X:
+                    if board[row_idx + 1][cell_idx] == X and board[row_idx + 2][cell_idx] == X:
+                        return X
+                    elif board[row_idx + 1][cell_idx - 1] and board[row_idx + 2][cell_idx - 2] == X:
+                        return X
+                        # TODO
+
+
+# def winner(board):
+#     """
+#     Returns the winner of the game, if there is one.
+#     """
+#     x_positions = set()
+#     zero_positions = set()
+#     for row_idx, row in enumerate(board):
+#         for cell_idx, cell in enumerate(row):
+#                 if cell == X:
+#                     x_positions.add((row_idx, cell_idx))
+#                 elif cell == O:
+#                     zero_positions.add((row_idx, cell_idx))
+#     return x_positions
+
+
+
+print(winner(board))
